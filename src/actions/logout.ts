b6abd256bd569cccdebccade9f6c43ee3185fd2e
Rohal Jamal <rohaljamal12345@gmail.com>
2025-01-31 16:38:21 +0500

    Initial commit

---
+++ src/actions/logout.ts
@@ -0,0 +1,23 @@
+// rrd imports
+import { redirect } from "react-router-dom";
+
+import { toast } from "react-toastify";
+
+// helpers
+import { deleteItem } from "../helpers";
+
+export async function logoutAction() {
+  // delete the user
+  deleteItem({
+    key: "userName"
+  })
+  deleteItem({
+    key: "budgets"
+  })
+  deleteItem({
+    key: "expenses"
+  })
+  toast.success("You've deleted your")
+  // return redirect
+  return redirect("/")
+}
